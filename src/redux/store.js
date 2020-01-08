2e6ad64f80da991d0a5f7f9eab4b8e8c24cf6b50
kydsib <simonas.kersulis@gmail.com>
2020-01-08 11:41:03 +0200

    Saving and loading data from LocalStorage works

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,23 @@ import { createStore, applyMiddleware } from 'redux'
 // using applyMiddleware and logger to see state changes logs
 import logger from 'redux-logger'
 
+import { loadState, saveState } from '../data_storage/localStorage'
 import rootReducer from './root-reducer'
 
 const middlewares = [logger]
 // middlewares expect array of values
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares))
+const persistedState = loadState()
+
+const store = createStore(
+	rootReducer,
+	persistedState,
+	applyMiddleware(...middlewares)
+)
+
+// saving state any time store state changes
+store.subscribe(() => {
+	saveState(store.getState())
+})
 
 export default store
