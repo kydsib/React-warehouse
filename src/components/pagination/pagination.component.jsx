bd422049539dd3567555d5f1b7d983eae8ddd302
kydsib <simonas.kersulis@gmail.com>
2020-02-25 18:00:58 +0200

    SingleItem class component changed to functional. Class -> hooks

--- src/components/pagination/pagination.component.jsx
+++ src/components/pagination/pagination.component.jsx
@@ -7,48 +7,45 @@ import Span from './span/span.component'
 
 import { itemsPerPage, currentPage } from '../../redux/item/item.actions'
 
-class PaginationStrip extends React.Component {
-	// Refactor w/ hookss
-	handleChange = e => {
+const PaginationStrip = ({ setItemsPerPage, setCurrentPage, arr }) => {
+	const handleChange = e => {
 		e.preventDefault()
 		let { value } = e.target
-		this.props.setItemsPerPage(value)
+		setItemsPerPage(value)
 	}
 
-	render() {
-		return (
-			<div className="container">
-				<div className="items-per-page-box">
-					<span>View </span>
-					{/* might need to set the default value */}
-					<select onChange={this.handleChange}>
-						<option value="5">5</option>
-						<option value="10">10</option>
-						<option value="25">25</option>
-						<option value="50">50</option>
-					</select>
-				</div>
-				<div className="select-page">
-					select page
-					{/* TODO
+	return (
+		<div className="container">
+			<div className="items-per-page-box">
+				<span>View </span>
+				{/* might need to set the default value */}
+				<select onChange={handleChange}>
+					<option value="5">5</option>
+					<option value="10">10</option>
+					<option value="25">25</option>
+					<option value="50">50</option>
+				</select>
+			</div>
+			<div className="select-page">
+				select page
+				{/* TODO
 				4. What would be the logic of showing pages if there are more of those 20 etc.
 				5. Use <select and options> istead of spans?
 				*/}
-					<div>
-						{this.props.arr.map(page => (
-							<Span
-								onClick={() => this.props.setCurrentPage(page)}
-								number={page + 1} // turiu perduoti arr?
-								key={page}
-								id={page}
-								value={page + 1}
-							/>
-						))}
-					</div>
+				<div>
+					{arr.map(page => (
+						<Span
+							onClick={() => setCurrentPage(page)}
+							number={page + 1} // turiu perduoti arr?
+							key={page}
+							id={page}
+							value={page + 1}
+						/>
+					))}
 				</div>
 			</div>
-		)
-	}
+		</div>
+	)
 }
 
 const mapDispatchToProps = dispatch => {
--- src/components/single-item/single-item.component.jsx
+++ src/components/single-item/single-item.component.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
+import { useDispatch } from 'react-redux'
 
 import {
 	updateValue,
@@ -11,24 +12,34 @@ import CustomButton from '../custom-button/custom-button.component'
 import Input from '../input-component/input.component'
 import './single-item.styles.scss'
 
-class SingleItem extends React.Component {
-	// Initializing State from Props In most cases, this is an antipattern. Don’t “copy props into state.” It creates a second source of truth for your data, which usually leads to bugs. One source of truth is best.
-	// However, it’s not an anti-pattern if you make it clear that the prop is only seed data for the component’s internally-controlled state.s
-
-	state = {
-		id: this.props.id,
-		name: this.props.name,
-		ean: this.props.ean,
-		type: this.props.type,
-		weight: this.props.weight,
-		color: this.props.color,
-		quantity: this.props.quantity,
-		price: this.props.price,
-		active: this.props.active,
+const SingleItem = ({
+	className,
+	id,
+	name,
+	ean,
+	type,
+	weight,
+	color,
+	active,
+	price,
+	quantity,
+	onClick,
+	deleteItem
+}) => {
+	const [params, setParams] = useState({
+		name,
+		id,
+		ean,
+		type,
+		weight,
+		color,
+		active,
+		price,
+		quantity,
 		time: ''
-	}
+	})
 
-	updateValue = e => {
+	const letsUpadate = e => {
 		e.preventDefault()
 		const currentDate = new Date()
 		const formatedDate =
@@ -43,105 +54,94 @@ class SingleItem extends React.Component {
 			currentDate.getMinutes() +
 			':' +
 			currentDate.getSeconds()
-		this.setState({ [e.target.name]: e.target.value, time: formatedDate })
+
+		setParams({
+			...params,
+			[e.target.name]: e.target.value,
+			time: formatedDate
+		})
 	}
 
-	render() {
-		const singleItem = this.state
-		return (
-			<tr className={`grid ${this.props.className}`}>
-				<td className="grid__item">
-					<span className="alignment">{this.props.name}</span>
-				</td>
-				<td className="grid__item">
-					<span className="alignment">{this.props.ean}</span>
-				</td>
-				<td className="grid__item">
-					<span className="alignment">{this.props.type}</span>
-				</td>
-				<td className="grid__item">
-					<span className="alignment">{this.props.weight}</span>
-				</td>
-				<td className="grid__item">
-					<span className="alignment">{this.props.color}</span>
-				</td>
-				<td className="grid-item">
-					<label className="alignment">
-						<input
-							type="checkbox"
-							name="active"
-							defaultChecked={this.props.active}
-							onClick={this.props.onClick}
-						/>
-					</label>
-				</td>
+	const dispatch = useDispatch()
 
-				{/* deciding if input fields needs to be shown in current page */}
-				{this.props.price === undefined ? null : (
-					<td>
-						<Input
-							onChange={this.updateValue}
-							id={this.props.id}
-							// value galeciau pasiimti per propsus o renderinti tikrindamas ar is mapState ateianti value skiriasi nuo prosps ir pakeisiti
-							value={singleItem.price}
-							name="price"
-							onBlur={() => {
-								this.props.updateInputValue(this.state)
-								this.props.updatePriceHistory(this.state)
-							}}
-						/>
-					</td>
-				)}
-				{this.props.quantity === undefined ? null : (
-					<td>
-						<Input
-							onChange={this.updateValue}
-							id={this.props.id}
-							value={singleItem.quantity}
-							name="quantity"
-							onBlur={() => {
-								this.props.updateInputValue(this.state)
-								this.props.updateQuantityHistory(this.state)
-							}}
-						/>
-					</td>
-				)}
+	return (
+		<tr key={id} className={`grid ${className}`}>
+			<td className="grid__item">
+				<span className="alignment">{name}</span>
+			</td>
+			<td className="grid__item">
+				<span className="alignment">{ean}</span>
+			</td>
+			<td className="grid__item">
+				<span className="alignment">{type}</span>
+			</td>
+			<td className="grid__item">
+				<span className="alignment">{weight}</span>
+			</td>
+			<td className="grid__item">
+				<span className="alignment">{color}</span>
+			</td>
+			<td className="grid-item">
+				<label className="alignment">
+					<input
+						type="checkbox"
+						name="active"
+						defaultChecked={active}
+						onClick={onClick}
+					/>
+				</label>
+			</td>
 
+			{/* deciding if input fields needs to be shown in current page */}
+			{price === undefined ? null : (
 				<td>
-					{/* checking if I need view button in current route */}
-					<Link to={`/products/${this.props.id}`}>
-						{this.props.quantity === undefined ? (
-							<CustomButton>VIEW</CustomButton>
-						) : null}
-					</Link>
-
-					<Link to={`/products/${this.props.id}/edit`}>
-						<CustomButton>EDIT</CustomButton>
-					</Link>
-
-					<Link to={`/products`}>
-						<CustomButton onClick={this.props.deleteItem}>
-							DELETE
-						</CustomButton>
-					</Link>
+					<Input
+						onChange={letsUpadate}
+						id={id}
+						// value galeciau pasiimti per propsus o renderinti tikrindamas ar is mapState ateianti value skiriasi nuo prosps ir pakeisiti
+						value={params.price}
+						name="price"
+						onBlur={() => {
+							dispatch(updateValue(params))
+							dispatch(updatePrice(params))
+						}}
+					/>
 				</td>
-			</tr>
-		)
-	}
+			)}
+			{quantity === undefined ? null : (
+				<td>
+					<Input
+						onChange={letsUpadate}
+						id={id}
+						value={params.quantity}
+						name="quantity"
+						onBlur={() => {
+							console.log('hehe')
+							dispatch(updateValue(params))
+							dispatch(updateQuantity(params))
+						}}
+					/>
+				</td>
+			)}
+
+			<td>
+				{/* checking if I need view button in current route */}
+				<Link to={`/products/${id}`}>
+					{quantity === undefined ? (
+						<CustomButton>VIEW</CustomButton>
+					) : null}
+				</Link>
+
+				<Link to={`/products/${id}/edit`}>
+					<CustomButton>EDIT</CustomButton>
+				</Link>
+
+				<Link to={`/products`}>
+					<CustomButton onClick={deleteItem}>DELETE</CustomButton>
+				</Link>
+			</td>
+		</tr>
+	)
 }
 
-const mapDispatchToProps = dispatch => {
-	return {
-		updateInputValue: item => {
-			dispatch(updateValue(item))
-		},
-		updateQuantityHistory: item => {
-			dispatch(updateQuantity(item))
-		},
-		updatePriceHistory: item => {
-			dispatch(updatePrice(item))
-		}
-	}
-}
-
-export default connect(null, mapDispatchToProps)(SingleItem)
+export default SingleItem
